9973769a7631e7f8eefc253482b72e18c9bfe88f
Louis Pazos <louispazos@gmail.com>
2018-01-11 17:19:15 +0000

    another example morphing from while to for loop

--- whileandforloop.js
+++ whileandforloop.js
@@ -1,5 +1,36 @@
 //While Loop and For Loop
 
+
+// From a While Loop to a For Loop II
+// Morph your previous while loop into a for loop that uses the same variable names. 
+//Remember, you’ll still need to declare the starting number of sheep and the number of months to print outside the loop. 
+//We’ve given you the starting number of sheep again, as well as the amount of months you’ll need to print out for use in the loop parameters. 
+//Here’s a solution for the previous while loop for reference:
+
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+
+while (monthNumber <= monthsToPrint) {
+    numSheep = numSheep * 4;
+    console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+    monthNumber++;
+}
+
+//  for Loops //
+
+var numSheep = 4;
+var monthsToPrint = 12;
+for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
+    numSheep *= 4
+    console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+}
+
+
+
+
+//Example 2//
+
 // These Dam Loops!
 // The Hoover Dam has 19 generators of multiple types. For simplicity, 
 //let’s say that the first 4 of these generators output 62 megawatts, 
