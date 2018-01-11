52cee498f93eaa71d3b15fd5e5dbd935075835a9
Louis Pazos <louispazos@gmail.com>
2018-01-11 05:09:29 +0000

    both loops has been completed

--- whileandforloop.js
+++ whileandforloop.js
@@ -1,1 +1,32 @@
 //While Loop and For Loop
+
+// These Dam Loops!
+// The Hoover Dam has 19 generators of multiple types. For simplicity, 
+//let’s say that the first 4 of these generators output 62 megawatts, 
+//and the other 15 output 124 megawatts. 
+//the Dam Rangers have asked you to design a system of two loops that turns each generator on in progression, 
+//and prints the new total of megawatts generated.
+
+// They’d like the first loop to be a while loop handling the first 4 generators. Then, 
+//they’d like the second loop to be a for loop that handles the other 15 generators. 
+//Each output line should resemble the following lines, with adjusted values for the currentGen and totalMW:
+
+// Generator #1 is on, adding 62 MW, for a total of 62 MW!
+// Generator #2 is on, adding 62 MW, for a total of 124 MW!
+
+var currentGen = 1;
+var totalGen = 19;
+var totalMW = 0;
+
+while (currentGen <= 4) {
+    totalMW += 62
+    console.log("Generator # " + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!");
+    currentGen++;
+}
+
+
+for (var i = 5; i <= 19; i++) {
+    totalMW += 124
+    console.log("Generator #" + i + " is on, adding 124 MW, for a total of " + totalMW + " MW!");
+    currentGen++;
+}
